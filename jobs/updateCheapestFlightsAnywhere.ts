83f3b73e5510a799f42eb1989880525907ea4825
Louisa Spicer <louisaspicer@gmail.com>
2023-08-24 15:25:04 +0100

    Change schema

---
+++ jobs/updateCheapestFlightsAnywhere.ts
@@ -0,0 +1,60 @@
+import { UpdateCheapestFlightsAnywhere, models } from "@teamkeel/sdk";
+import fetch from "node-fetch";
+
+const randomDestinationEntityIds = [
+  "29475383",
+  "29475385",
+  "29475229",
+  "29475260",
+  "29475381",
+  "29475369",
+  "29475373",
+  "29475389",
+  "29475349",
+];
+
+export default UpdateCheapestFlightsAnywhere(async (ctx) => {
+  const res = await fetch("/apiservices/v3/flights/live/search/create", {
+    method: "POST",
+    headers: { "x-api-key": ctx.secrets.SKYSCANNER_PUBLIC_KEY },
+    body: {
+      query: {
+        market: "UK",
+        locale: "en-GB",
+        currency: "GBP",
+        queryLegs: [
+          {
+            originPlaceId: {
+              entityId: "29475375",
+            },
+            destinationPlaceId: {
+              entityId: "29475383",
+            },
+            date: {
+              year: 2023,
+              month: 8,
+              day: 25,
+            },
+          },
+        ],
+        cabinClass: "CABIN_CLASS_UNSPECIFIED",
+        adults: 1,
+        nearbyAirports: true,
+      },
+    },
+  });
+
+  if (!res.ok) {
+    throw new Error(`Failed to fetch. Status: ${res.status}`);
+  }
+
+  let results;
+  const contentType = res.headers.get("Content-Type");
+  if (contentType && contentType.includes("application/json")) {
+    results = await res.json();
+  } else {
+    results = await res.text();
+  }
+
+  console.log({ results });
+});
